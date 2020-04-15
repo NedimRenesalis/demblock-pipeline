4819bf43c9322f69bc2d0c5e614f5c5c4a732e41
Ramiz Polić <ramiz.polic@hotmail.com>
2020-04-15 19:11:05 +0200

    infra: disable cert manager

--- k8s/k8s_dep.sh
+++ k8s/k8s_dep.sh
@@ -3,12 +3,12 @@
 echo "Deploying required k8s dependencies..."
 # ===================================================
 # CERT MANAGER
-echo "Deploying cert manager..."
-kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager-legacy.crds.yaml
-kubectl create namespace cert-manager || echo "Namespace exists."
-helm repo add jetstack https://charts.jetstack.io
-helm repo update
-helm upgrade cert-manager --install --wait --timeout 15m0s --atomic --namespace cert-manager jetstack/cert-manager --version v0.14.1
+# echo "Deploying cert manager..."
+# kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager-legacy.crds.yaml
+# kubectl create namespace cert-manager || echo "Namespace exists."
+# helm repo add jetstack https://charts.jetstack.io
+# helm repo update
+# helm upgrade cert-manager --install --wait --timeout 15m0s --atomic --namespace cert-manager jetstack/cert-manager --version v0.14.1
 
 # ===================================================
 # CLUSTER ISSUER
