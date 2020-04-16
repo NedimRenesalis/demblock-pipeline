624c2a032590a4e696cfdba77de704b2f17d6a58
Ramiz Polić <ramiz.polic@hotmail.com>
2020-04-16 21:36:16 +0200

    k8s: add cluster issuer for cert manager

--- k8s/k8s_dep.sh
+++ k8s/k8s_dep.sh
@@ -3,14 +3,15 @@
 echo "Deploying required k8s dependencies..."
 # ===================================================
 # CERT MANAGER - needed for NGINX INGRESS, we are using GCE Ingress
-# echo "Deploying cert manager..."
-# kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager-legacy.crds.yaml
-# kubectl create namespace cert-manager || echo "Namespace exists."
-# helm repo add jetstack https://charts.jetstack.io
-# helm repo update
-# helm upgrade cert-manager --install --wait --timeout 15m0s --atomic --namespace cert-manager jetstack/cert-manager --version v0.14.1
+echo "Deploying cert manager..."
+kubectl apply --validate=false -f https://github.com/jetstack/cert-manager/releases/download/v0.14.1/cert-manager-legacy.crds.yaml
+kubectl create namespace cert-manager || echo "Namespace exists."
+helm repo add jetstack https://charts.jetstack.io
+helm repo update
+helm upgrade cert-manager --install --wait --timeout 15m0s --atomic --namespace cert-manager jetstack/cert-manager --version v0.14.1
 
 # ===================================================
 # CLUSTER ISSUER
-echo "Deploying Certificates..."
-python3 ./k8s/generate_certs.py | kubectl apply -f -
+echo "Deploying ClusterIssuer..."
+kubectl apply -f ./k8s/prod-cm.yaml
+# python3 ./k8s/generate_certs.py | kubectl apply -f -
